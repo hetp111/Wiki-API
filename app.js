2bfeb77d56ae38864deb86c277900d929a40bd03
Het <hetp111@users.noreply.github.com>
2021-03-16 14:33:45 +0530

    GET and POST done

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB",{useNewUrlParser:true,useUnifiedTopology: true});
+
+const articleSchema = {title:String, content:String};
+const Article = mongoose.model("Article",articleSchema);
+
+//TODO
+app.get("/articles",function(req,res){
+    Article.find(function(err,foundArticles){
+        if(!err)res.send(foundArticles);
+        else res.send(err);
+    });
+});
+
+app.post("/articles",function(req,res){
+    const newArticle = new Article({
+        title:req.body.title,
+        content:req.body.content
+    });
+    newArticle.save(function(err){
+        if(!err)res.send("Added the new article!");
+        else res.send(err);
+    });
+});
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+});
