097398318e16bc8cfa44f6a9e79f2253b4e06d78
Het <hetp111@users.noreply.github.com>
2021-03-16 21:42:15 +0530

    Add"ED" GET & PUT

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const ejs = require("ejs");
+//const ejs = require("ejs");
 const mongoose = require('mongoose');
 
 const app = express();
 
-app.set("view engine", "ejs");
+//app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+//app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -46,6 +46,23 @@ app.route("/articles")
         }
     );
 
+app.route("/articles/:articleTitle")
+    .get(
+        function(req,res){
+            Article.findOne({title:req.params.articleTitle},function(err, foundArticle){
+                if(foundArticle)res.send(foundArticle);
+                else res.send("Not found.");
+            });
+        }
+    )
+    .put(
+        function(req,res){
+            Article.updateOne({title:req.params.articleTitle},{title:req.body.title,content:req.body.content},function(err,docs){
+                if(!err)res.send(req.body);
+                else res.send(err);
+            });
+        }
+    );
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
